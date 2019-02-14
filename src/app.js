de161dc099918bf70d2a8bd1fa03178df3aa0b6a
Christopher Lamm <chris@theantichris.com>
2019-02-14 10:12:23 -0500

    Generating more blocks.

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ const Blockchain = require('./Blockchain')
 
 const chain = new Blockchain()
 
-console.log('Mining block...')
-chain.addBlock('This is block 1')
-
-console.log('Mining block')
-chain.addBlock('This is block 1')
+for (let i = 1; i <= 10; i++) {
+  console.log('Mining block...')
+  chain.addBlock(`Block ${i} data`)
+}
 
 console.log(JSON.stringify(chain, null, 4))
 console.log('Is blockchain valid? ' + chain.isValid().toString())
